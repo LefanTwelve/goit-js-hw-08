51da6f7e19500f1c2f65124ad0d4223f24c7f496
Aleksey Krupskyi <108680778+LefanTwelve@users.noreply.github.com>
2022-12-14 17:37:26 +0200

    update gallery.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const galleryContainer = document.querySelector('.gallery');
+
+function createGalleryItems ({ preview, original, description}) {
+    return `
+      <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/>
+        </a>
+      </div>
+    `;
+}
+
+const addGalleryItems = galleryItems.map(createGalleryItems).join('');
+galleryContainer.insertAdjacentHTML ('beforeend', addGalleryItems);
+
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250, scrollZoom: true, captionsData: "alt" });
 
 console.log(galleryItems);
